31d77daef33594e7f615618f9cef162bcf8d8179
shindekalpesharun <shindekalpesharun@gmail.com>
2021-08-09 23:21:28 +0530

    bottom navgation sample done

--- app/src/main/java/com/shindejayesharun/composebottomnavigationbar/MainActivity.kt
+++ app/src/main/java/com/shindejayesharun/composebottomnavigationbar/MainActivity.kt
@@ -6,11 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.shindejayesharun.composebottomnavigationbar.ui.theme.ComposeBottomNavigationBarTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +27,7 @@ class MainActivity : ComponentActivity() {
             ComposeBottomNavigationBarTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
@@ -46,7 +53,7 @@ fun TopBarPreview() {
 
 
 @Composable
-fun BottomNavigationBar() {
+fun BottomNavigationBar(navController: NavController) {
     val items = listOf(
         NavigationItem.Home,
         NavigationItem.Book,
@@ -57,9 +64,22 @@ fun BottomNavigationBar() {
         backgroundColor = colorResource(id = R.color.design_default_color_primary_dark),
         contentColor = Color.White
     ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
         items.forEach { item ->
             BottomNavigationItem(
-                selected = false, onClick = { /*TODO*/ },
+                selected = currentRoute == item.route, onClick = {
+                    navController.navigate(item.route){
+                        navController.graph.startDestinationRoute?.let { route->
+                            popUpTo(route = route){
+                                saveState = true
+                            }
+
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    }
+                },
                 enabled = true,
                 icon = { Icon(painterResource(id = item.icon), contentDescription = item.route) },
                 label = { Text(text = item.title) }
@@ -72,19 +92,39 @@ fun BottomNavigationBar() {
 @Preview(showBackground = true)
 @Composable
 fun BottomNavigationBarPreview() {
-    BottomNavigationBar()
+   // BottomNavigationBar(navController = NavController(this))
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    Scaffold(
+        topBar = { TopBar() },
+        bottomBar = { BottomNavigationBar(navController = navController) }
+    ) {
+        Navigation(navHostController = navController)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposeBottomNavigationBarTheme {
-        Greeting("Android")
+fun MainScreenPreview() {
+    MainScreen()
+}
+
+@Composable
+fun Navigation(navHostController: NavHostController) {
+    NavHost(navHostController, startDestination = NavigationItem.Home.route) {
+        composable(NavigationItem.Home.route) {
+            Text("Home")
+        }
+        composable(NavigationItem.Book.route) {
+            Text("Book")
+        }
+        composable(NavigationItem.Profile.route) {
+            Text("Profilw")
+        }
     }
 }
 
